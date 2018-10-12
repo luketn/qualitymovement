a7ecb14d5c647d8ab947d376fde1cc62e5aca0ec
Luke Thompson <endeavour9@gmail.com>
2018-10-13 09:20:44 +1100

    Update cfn-deploy.sh

--- scripts/cfn-deploy.sh
+++ scripts/cfn-deploy.sh
@@ -2,12 +2,12 @@
 
 SUBDOMAIN=$1
 
-echo "Started CloudFormation deployment of "$SUBDOMAIN".mycodefu.com at "`date`"..."
-aws cloudformation deploy --region us-east-1 --template-file ../template.yml --stack-name $SUBDOMAIN \
+echo "Started CloudFormation deployment of qualitymovement.org at "`date`"..."
+aws cloudformation deploy --region us-east-1 --template-file ../template.yml --stack-name site \
         --parameter-overrides \
-            "HostedZoneName=mycodefu.com." \
-            "WebsiteAddress="$SUBDOMAIN".mycodefu.com" \
-            "S3BucketName="$SUBDOMAIN"-site" \
-            "TlsCertificateArn=arn:aws:acm:us-east-1:204244381428:certificate/47534d52-e75c-40e0-b4ed-c4a8d0a62ba6"
+            "HostedZoneName=qualitymovement.org." \
+            "WebsiteAddress=qualitymovement.org" \
+            "S3BucketName="qualitymovement-site" \
+            "TlsCertificateArn=arn:aws:acm:us-east-1:204244381428:certificate/ee0c4535-d0b9-455f-8d9e-f8b9b338eb49"
 echo "Finished CloudFormation deploy at "`date`
 
